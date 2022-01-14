6168fb765b98eaf03de4664c3d37be790f8e1574
cnelson20 <cjnelson2004@gmail.com>
2022-01-14 02:16:27 +0000

    console

--- console.c
+++ console.c
@@ -11,6 +11,15 @@
 int main(int argc, char *argv[]) {
 	int ch;
 	
+	struct stat std;
+	char *copy, *temp;
+	int max_x, max_y, max_line_strlen = 0;
+	int x,y;
+	int scroll_x, scroll_y;
+	int max_scroll_y, max_scroll_x;
+	int i, fd;
+	char **lines;
+	
 	initscr();
 	raw();
 	keypad(stdscr,TRUE);
@@ -19,33 +28,27 @@ int main(int argc, char *argv[]) {
 	idlok(stdscr,TRUE);
 	scrollok(stdscr,TRUE);
 	
-	int maxx, maxy;
-	getmaxyx(stdscr, maxy, maxx);
-
-	struct stat std;
 	stat(argv[1],&std);
-	char *copy = malloc(std.st_size);
-	int fd = open(argv[1],O_RDONLY);
+	copy = malloc(std.st_size);
+	fd = open(argv[1],O_RDONLY);
 	read(fd,copy,std.st_size);
 	close(fd);
-	char *temp = copy;
+	temp = copy;
 	
-	int i;
 	for (i = 0; *temp; temp++) {
 		if (*temp == '\n') {i++;}
 	}
-	char **lines = malloc((i+1)*sizeof(char *));
+	lines = malloc((i+1)*sizeof(char *));
 	temp = copy;
 	for (i = 0; temp && *temp; i++) {
 		char *curr = strsep(&temp,"\n");
 		lines[i] = curr;
 	}
 	lines[i] = NULL;
-	int max_scroll_y = i;
-	int max_scroll_x = 0;
+	max_scroll_y = i;
+	max_scroll_x = 0;
 	
 
-	int max_x, max_y, max_line_strlen = 0;
 	getmaxyx(stdscr, max_y, max_x);
 	for (i = 0; lines[i]; i++) {
 		printw("lines[%d]: '%s'\n",i,lines[i]);
@@ -57,16 +60,17 @@ int main(int argc, char *argv[]) {
 	} else {
 		max_scroll_y = 0;
 	}
-	int scroll_x, scroll_y;
 	scroll_x = 0;
 	scroll_y = 0;
 	printw("max_x: %d  max_y: %d\n", max_x, max_y);
 	printw("max_scroll_x: %d  max_scroll_y: %d\n", max_scroll_x, max_scroll_y);
 	printw("scroll_x: %d  scroll_y: %d\n", scroll_x, scroll_y);
 	
-	int x,y;
 	getyx(stdscr, y ,x);
 	while(1) {
+		int old_scroll_x = scroll_x;
+		int old_scroll_y = scroll_y;
+		
 		ch = getch();
 		if (ch >= 0x20 && ch < 0x7F) {
 			break;
@@ -104,15 +108,29 @@ int main(int argc, char *argv[]) {
 					break;
 			}
 		}
-		
-		clear();
-		move(0,0);
-		int i,j;
-		for (j = scroll_y; j < scroll_y + max_y && lines[j]; j++) {
-		  for (i = scroll_x; i < scroll_x + max_x && i < strlen(lines[j]) && lines[j][i]; i++) {
-				addch(lines[j][i]);
+		if (scroll_x == old_scroll_x) {
+			if (scroll_y != old_scroll_y) {
+				if (scroll_y > old_scroll_y) {
+					while (scroll_y > old_scroll_y) {
+						scroll(1);
+						old_scroll_y++;
+					}
+				} else {
+					while (scroll_y < old_scroll_y) {
+						scroll(-1);
+						old_scroll_y--;
+					}
+				}
 			}
-			addch('\n');
+		} else {
+			move(0,0);
+			int i,j;
+			for (j = scroll_y; j < scroll_y + max_y && lines[j]; j++) {
+			for (i = scroll_x; i < scroll_x + max_x && i < strlen(lines[j]) && lines[j][i]; i++) {
+					addch(lines[j][i]);
+				}
+				addch('\n');
+			}
 		}
 		move(y,x);
 		refresh();		
