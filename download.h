345e5ca407f9d1aa99bd78954b98f714bc99f742
cnelson20 <cjnelson2004@gmail.com>
2022-01-19 11:37:44 -0500

    download.h

---
+++ download.h
@@ -0,0 +1,12 @@
+#define IMPORTANT(x) (x == ELEMENT_BUTTON || x == ELEMENT_INPUT || x == ELEMENT_TEXTAREA || x == ELEMENT_SELECT)
+
+/* Vars */
+extern char curl_strings[][16];
+
+struct form_args_holder {
+  char **args;
+  size_t length;
+};
+
+char *curl(char *site, char *path, int method /* 1 = get, 0 = post*/, char **form_data);
+struct form_args_holder *post_check(struct html_element * current, struct html_element * form ,struct form_args_holder *gen);
